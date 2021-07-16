dc431f5af12eea5720c1f077c1b3a3e9fb8d7f7a
robz <robenkaraa29@gmail.com>
2021-07-16 12:25:11 +0200

    rays are now spawned in 32x32 packets, like 4-8% speedup due to less cache misses

--- src/World.cpp
+++ src/World.cpp
@@ -26,14 +26,30 @@ Canvas render( const Camera& cam , const World &world )
 #pragma omp parallel default(none) shared(cam, world , canvas)  num_threads(OMP_NUM_THREADS)
     {
 #pragma omp for schedule(dynamic,10) // 10 magic number, experiment with this
+
+        // 32x32 ray packets in an attempt to improve performance through cache access improvement since
+        // closer rays are more likely to intersect the same objects hence may have same memory accesses
+
         for ( int y = 0 ; y < cam.getVSize() ; ++y )
         {
-            for ( int x = 0 ; x < cam.getHSize() ; ++x )
+            const auto upperBoundY = (cam.getVSize() - 32 - y >= 0 ? 32 : cam.getVSize() - 32 - y ) ;
+
+            for ( int x = 0 ; x < cam.getHSize(); ++x )
             {
-                auto ray = cam.getRayForPixel( x , y );
-                Color3f c = Lighting::color_at(world, ray, true , 5 );
-                canvas.write( c , x , y );
+                const auto upperBoundX = (cam.getHSize() - 32 - x >= 0 ? 32 : cam.getHSize() - 32 - x ) ;
+
+                for ( int y_inside = 0 ; y_inside < upperBoundY ; ++y_inside )
+                {
+                    for (int x_inside = 0 ; x_inside < upperBoundX ; ++x_inside )
+                    {
+                        auto ray = cam.getRayForPixel( x + x_inside , y + y_inside );
+                        Color3f c = Lighting::color_at(world, ray, true , 5 );
+                        canvas.write( c , x + x_inside  , y + y_inside );
+                    }
+                }
+                x += upperBoundX-1; // -1 to account for ++x
             }
+            y += upperBoundY-1;
         }
     }
 
